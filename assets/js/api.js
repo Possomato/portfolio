83a8cffa27b43e99959fd62b44c627ba78094add
Gabriel Possomato <gabrielb.possomato@gmail.com>
2023-07-07 10:23:59 -0300

    att

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData(){
-  const url = 'https://raw.githubusercontent.com/Possomato/DIO/main/javascript/Bootcamp-FormacaoJavaScriptDeveloper/modulo04/portifolio/data/profile.json'
+  const url = 'https://raw.githubusercontent.com/Possomato/portfolio/main/data/profile.json'
   const fetching = await fetch(url)
   return await fetching.json()
 }
